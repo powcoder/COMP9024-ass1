2ee4c5434a3ac2b92a174b16b02885628a442f37
powcoder <powcoder@163.com>
2022-12-16 11:30:12 +0800

    COMP9024 ass1

---
+++ src/studentRecord.h
@@ -0,0 +1,21 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+// Student record header file ... Assignment 1 COMP9024 17s2
+// DO NOT CHANGE
+
+typedef struct {
+   int   zID;
+   int   credits;
+   float WAM;
+} studentRecordT;
+
+int   readValidID(void);
+int   readValidCredits(void);
+float readValidWAM(void);
+void  printStudentData(int, int, float);
